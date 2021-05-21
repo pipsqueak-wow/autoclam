9d3b039b26652e0954f46f0ee8ee4949646f0fec
Pipsqueak <pipsqueak.pyrewood@gmail.com>
2021-05-21 11:59:48 +0300

    Delay ClamScan (tm) until leaving combat if triggered when in combat.

--- AutoClam.lua
+++ AutoClam.lua
@@ -6,16 +6,36 @@ local clamIDs = {
   [15874] = true, -- Soft-shelled Clam
   [24476] = true  -- Jaggal Clam
 }
+local clamScanQueued = false
 
 function AutoClam:OnInitialize()
-  self:RegisterEvent("BAG_UPDATE","BagUpdate")
+  self:RegisterEvent("BAG_UPDATE_DELAYED","BagUpdateDelayed")
+  self:RegisterEvent("PLAYER_REGEN_ENABLED","PlayerRegenEnabled")
 end
 
-function AutoClam:BagUpdate()
+function AutoClam:BagUpdateDelayed()
+  if not InCombatLockdown() then
+    self:ClamScan()
+  else
+    clamScanQueued = true
+  end
+end
+
+function AutoClam:PlayerRegenEnabled()
+  if clamScanQueued then
+    clamScanQueued = false
+    self:ClamScan()
+  end
+end
+
+function AutoClam:ClamScan()
   for bag = 4,0,-1 do
     for slot = GetContainerNumSlots(bag),1,-1 do
-      local itemId = GetContainerItemID(bag, slot)
-      if clamIDs[itemId] then
+      local _, _, locked, _, _, _, _, _, _, itemId = GetContainerItemInfo(bag, slot)
+      -- Check for clams being locked to avoid a potential error but that happening seems like such
+      -- an edge case that we can't be bothered to queue an extra check for when it's unlocked.
+      -- It will be opened the next time something happens in the bag anyway.
+      if clamIDs[itemId] and not InCombatLockdown() and not locked then
         UseContainerItem(bag, slot)
       end
     end
---
+++ CHANGES.md
@@ -0,0 +1,9 @@
+# 1.0.1
+
+### Bug Fixes
+
+* Delay ClamScan (tm) until leaving combat if triggered when in combat.
+
+# 1.0
+
+* Initial public release
