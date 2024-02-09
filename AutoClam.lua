d75fe97302a0831b0687a37bf1a6399312c5b4cc
Road-block <dridzt.addons@hotmail.com>
2024-02-09 18:44:46 +0200

    cast revive on AutoClam for Season of Discovery

--- AutoClam.lua
+++ AutoClam.lua
@@ -4,7 +4,7 @@ local clamIDs = {
   [5524] = true,  -- Thick-shelled Clam
   [7973] = true,  -- Big-mouth Clam
   [15874] = true, -- Soft-shelled Clam
-  [24476] = true  -- Jaggal Clam
+  --[24476] = true  -- Jaggal Clam -- TBC
 }
 local clamScanQueued = false
 
@@ -16,7 +16,7 @@ end
 function AutoClam:SlotsFree()
   local totalFree, freeSlots, bagFamily = 0
   for bag = BACKPACK_CONTAINER, NUM_BAG_SLOTS do
-    freeSlots, bagFamily = GetContainerNumFreeSlots(bag)
+    freeSlots, bagFamily = C_Container.GetContainerNumFreeSlots(bag)
     if ( bagFamily == 0 ) then -- not a specialty bag
       totalFree = totalFree + freeSlots
     end
@@ -52,14 +52,17 @@ end
 
 function AutoClam:ClamScan()
   for bag = NUM_BAG_SLOTS,BACKPACK_CONTAINER,-1 do
-    for slot = GetContainerNumSlots(bag),1,-1 do
-      local _, _, locked, _, _, _, _, _, _, itemId = GetContainerItemInfo(bag, slot)
-      -- Check for clams being locked to avoid a potential error but that happening seems like such
-      -- an edge case that we can't be bothered to queue an extra check for when it's unlocked.
-      -- It will be opened the next time something happens in the bag anyway.
-      if clamIDs[itemId] and not (InCombatLockdown() or locked) then
-        self:RegisterEvent("LOOT_READY")
-        UseContainerItem(bag, slot)
+    for slot = C_Container.GetContainerNumSlots(bag),1,-1 do
+      local slotInfo = C_Container.GetContainerItemInfo(bag, slot)
+      if slotInfo and slotInfo.hasLoot then
+        local itemID, isLocked = slotInfo.itemID, slotInfo.isLocked
+        -- Check for clams being locked to avoid a potential error but that happening seems like such
+        -- an edge case that we can't be bothered to queue an extra check for when it's unlocked.
+        -- It will be opened the next time something happens in the bag anyway.
+        if clamIDs[itemID] and not (InCombatLockdown() or isLocked) then
+          self:RegisterEvent("LOOT_READY")
+          C_Container.UseContainerItem(bag, slot)
+        end
       end
     end
   end
